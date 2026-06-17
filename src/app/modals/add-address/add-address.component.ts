a0418551f11ba8976f4a8075cd139832ac11887e
Naveen <zinivd.cloud@gmail.com>
2026-06-17 19:21:58 +0530

    layout first commit

---
+++ src/app/modals/add-address/add-address.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-address',
+  imports: [],
+  templateUrl: './add-address.component.html',
+  styleUrl: './add-address.component.css'
+})
+export class AddAddressComponent {
+
+}
---
+++ src/app/modals/add-phone-number/add-phone-number.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-phone-number',
+  imports: [],
+  templateUrl: './add-phone-number.component.html',
+  styleUrl: './add-phone-number.component.css'
+})
+export class AddPhoneNumberComponent {
+
+}
